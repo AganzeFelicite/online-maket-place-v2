f7fbd3fec9ec71c0c1fb1e945bf0c5eadfa1489e
AganzeFelicite <aganzefelicite@gmail.com>
2025-04-23 12:44:50 +0200

    api documentations and authorization in swagger

--- src/main/kotlin/com/online_market_place/online_market_place/controller/auth/AuthController.kt
+++ src/main/kotlin/com/online_market_place/online_market_place/controller/auth/AuthController.kt
@@ -4,6 +4,7 @@ import com.online_market_place.online_market_place.dto.auth.AuthResponse
 import com.online_market_place.online_market_place.dto.user.LoginRequest
 import com.online_market_place.online_market_place.dto.user.UserRegisterRequest
 import com.online_market_place.online_market_place.service.service_interface.AuthService
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -19,17 +20,20 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(private val authService: AuthService) {
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user")
     fun register(@Valid @RequestBody request: UserRegisterRequest): ResponseEntity<String> {
         return ResponseEntity.ok(authService.register(request))
     }
 
     @GetMapping("/verify-email")
+    @Operation(summary = "Verify email address")
     fun verifyEmail(@RequestParam token: String): ResponseEntity<String> {
         val message = authService.verifyToken(token)
         return ResponseEntity.ok(message)
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login user ")
     fun login(@Valid @RequestBody request: LoginRequest): ResponseEntity<AuthResponse> {
         return ResponseEntity.ok(authService.login(request))
     }
