bd9de8faa25f28720062887e970327114cb05e6d
AganzeFelicite <aganzefelicite@gmail.com>
2025-04-24 17:34:09 +0200

    project cleanup, and restructuring of the project

--- src/main/kotlin/com/online_market_place/online_market_place/common/exception/ValidationException.kt
+++
@@ -1,9 +0,0 @@
-package com.online_market_place.online_market_place.common.exception
-
-/**
- * Validation exception
- */
-class ValidationException(
-    override val message: String,
-    override val errorCode: String = "VALIDATION_ERROR"
-) : BaseException(message, errorCode)
--- src/main/kotlin/com/online_market_place/online_market_place/user/enum_/UserRole.kt
+++ src/main/kotlin/com/online_market_place/online_market_place/user/enum_/UserRole.kt
@@ -1,5 +1,6 @@
 package com.online_market_place.online_market_place.user.enum_
 
+@Suppress("unused")
 enum class UserRole {
     ADMIN,
     CUSTOMER,
